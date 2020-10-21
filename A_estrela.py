ded6881c74c16310acc8abd7ac954a736749bcf9
Guilherme Chichanoski <GChicha@users.noreply.github.com>
2020-10-21 11:19:46 -0300

    Update A_estrela.py

--- A_estrela.py
+++ A_estrela.py
@@ -3,32 +3,32 @@ from copy import deepcopy
 import heapq
 import time
 
+tabuleiroFinal=[1,2,3,4,12,13,14,5,11,0,15,6,10,9,8,7]
+
 class Peca():
-    def __init__(self,tabuleiro,pai=None,g=0):
-        self.tabuleiro =tabuleiro
-        self.pai=pai
+    def __init__(self, tabuleiro, pai = None, g = 0):
+        self.tabuleiro = tabuleiro
+        self.pai = pai
         self.g = g
-        self.calcH()
-        self.calcF()
+        self.f = g + heuristicaUm(tabuleiro)
+
     #def __str__(self):
         #return "g = {} , h = {} , f = {} , pai = {},tabuleiro={}".format(self.g,self.h,self.f,self.pai,self.tabuleiro)
+
     def __repr__(self):
         return "{}".format(self.tabuleiro)
+
     def __eq__(self, other):
-        return self.tabuleiro==other.tabuleiro 
+        return self.tabuleiro == other.tabuleiro
+
     def __lt__(self,other):
-        return self.f<other.f
+        return self.f < other.f
+
     def __gt__(self, other):
-        return self.f>other.f
-    def calcH(self):
-        self.h = heuristicaUm(self.tabuleiro)
-    def calcF(self):
-        self.f = self.g + self.h
+        return self.f > other.f
+
     def __hash__(self):
         return hash(str(self.tabuleiro))
-#def caminho(tabuleiro,inicio,fim):
-
-tabuleiroFinal=[1,2,3,4,12,13,14,5,11,0,15,6,10,9,8,7]
 
 def readInput():
     #entrada = list(map(int, input().split()))
@@ -43,15 +43,19 @@ def heuristicaUm(auxTab):
         if auxTab[a]!=tabuleiroFinal[a]:
             count+=1
     return  count
+
 def geraSucessores(noPai):
     filhos=[]
     for index,it in enumerate(noPai.tabuleiro):
         if it == 0:
-            indexNulo=index
+            indexNulo = index
     if indexNulo>=4:
-        filhoCima=Peca(deepcopy(noPai.tabuleiro),noPai,noPai.g+1)
-        filhoCima.tabuleiro[indexNulo], filhoCima.tabuleiro[indexNulo-4] = filhoCima.tabuleiro[indexNulo-4], 0 
-        filhos.append(filhoCima)
+        # Usa essa sintaxe de copia de vetor [:] melhor que copy
+        # Verifica se já existe no listaFechada antes de calcular a heuristica, da para economizar um tempo aqui
+        filhoCima=Peca(tabuleiro[:],noPai,noPai.g+1)
+        filhoCima.tabuleiro[indexNulo], filhoCima.tabuleiro[indexNulo-4] = filhoCima.tabuleiro[indexNulo-4], 0
+        # Usa yield no lugar de retornar uma lista, a sintaxe fica melhor
+        yield filhoCima
     if indexNulo<12:
         filhoBaixo=Peca(deepcopy(noPai.tabuleiro),noPai,noPai.g+1)
         filhoBaixo.tabuleiro[indexNulo], filhoBaixo.tabuleiro[indexNulo+4] = filhoBaixo.tabuleiro[indexNulo+4], 0
@@ -76,31 +80,22 @@ def geraSucessores(noPai):
 
 def AEstrela(noI):
     #start=time.process_time()
-    aux ={}
-    listaAberta=[]        #heapq 
-    listaFechada=set(aux)   #set
-    heapq.heappush(listaAberta,noI)
-    selecionado = listaAberta[0]
-    while(len(listaAberta)!=0) and selecionado.tabuleiro!=tabuleiroFinal:
-        listaFechada.add(selecionado)                        
-        heapq.heappop(listaAberta)                              #pop heap min
-        filhos = geraSucessores(selecionado)                    
-        for filho in filhos:
-            if filho in listaFechada:
-                continue
-            if filho in listaAberta:
-                for item in listaAberta:             
-                        if item==filho:
-                            if filho>item: 
-                                continue
-            heapq.heappush(listaAberta,filho)
-        selecionado=listaAberta[0]
-    return selecionado.g
 
+    listaAberta = []        #heapq 
+    listaFechada = set()   #set
+
+    heapq.heappush(listaAberta,noI)
+
+    while (selecionado := heapq.heappop(listaAberta)) != tabuleiroFinal:
+        listaFechada.add(selecionado)                        
+        [heapq.heappush(listaAberta,filho) for filho in geraSucessores(selecionado) if filho not in listaFechada]
+
+    return selecionado.g
 
 def main():
     inicial = Peca(readInput())
     resultado=AEstrela(inicial)
     print(resultado)
+
 if __name__ == '__main__':
     main()
